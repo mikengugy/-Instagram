afe6adba57563dfd5d00674d0bb83467e2e133f3
mikengugy <mikengugy@gmail.com>
2020-11-25 14:31:51 +0300

    added the form file with comment form

---
+++ insta/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comment, Post
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['user','image','date_commented']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model  = Post
+        fields = ['image', 'caption']
